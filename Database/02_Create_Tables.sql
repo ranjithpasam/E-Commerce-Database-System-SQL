1ff395c65467ea1b508c6fd847ffe88db5427157
P Ranjith Kumar <ranjithpasam2230@gmail.com>
2026-07-28 11:33:38 +0530

    Add Products table

--- Database/02_Create_Tables.sql
+++ Database/02_Create_Tables.sql
@@ -24,3 +24,26 @@ CREATE TABLE Brands
     Country VARCHAR(100)
 );
 GO
+
+---------------------------------------------------
+-- Products Table
+---------------------------------------------------
+
+CREATE TABLE Products
+(
+    ProductID INT IDENTITY(1,1) PRIMARY KEY,
+    CategoryID INT,
+    BrandID INT,
+    ProductName VARCHAR(150) NOT NULL,
+    Description VARCHAR(500),
+    Price DECIMAL(10,2) NOT NULL,
+    StockQuantity INT DEFAULT 0,
+
+    FOREIGN KEY (CategoryID)
+        REFERENCES Categories(CategoryID),
+
+    FOREIGN KEY (BrandID)
+        REFERENCES Brands(BrandID)
+);
+GO
+
